07db641f47ce34d8279bc0981e92b65f2816a6d1
Jacky Zheng <30736593+jackywzheng@users.noreply.github.com>
2020-09-09 21:28:37 -0700

    Added middleware

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
 const express = require('express');
 const path = require('path');
+const cookieSession = require('cookie-session');
+
+const FeedbackService = require('./services/FeedbackService');
+const SpeakersService = require('./services/SpeakerService');
+
+const feedbackService = new FeedbackService('./data/feedback.json');
+const speakersService = new SpeakersService('./data/speakers.json');
+
+const routes = require('./routes');
 
 const app = express();
 
 // by convention 8080 normally, but express convention is 3000
 const port = 3000;
 
+app.set('trust proxy', 1);
+
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['dwadawfaw', 'fhuiwa98494'],
+  })
+);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
 app.use(express.static(path.join(__dirname, './static')));
 
-app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname, './static/index.html'));
-});
-
-app.get('/speakers', (request, response) => {
-  response.sendFile(path.join(__dirname, './static/speakers.html'));
-});
+app.use('/', routes({ feedbackService, speakersService }));
 
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}!`);
